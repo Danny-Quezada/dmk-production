810b55f5f34d38c8ba37184621bfa56ac0e58d26
Kevin-Roa04 <kevin.roa49u@std.uni.edu.ni>
2024-06-10 13:44:54 -0600

    Adding html in ZI (page)

--- src/pages/User/Calculator/ZeroInventory.tsx
+++ src/pages/User/Calculator/ZeroInventory.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../../components/Button/Button'
+import TextField from '../../../components/Textfield/TextField'
 
 const ZeroInventory = () => {
-  return (
-    <div>
+    const [ZI, setZI] = useState({
+        PPT: "",
+        MESES: "",
       
-    </div>
+      });
+    
+
+    const onChange = (e) => {
+        setZI({ ...ZI, [e.target.name]: e.target.value });
+      };
+    
+      const submit = (event) => {
+        event.preventDefault();
+      
+        // changeResult(Math.sqrt((2 * eoq.D * eoq.S) / eoq.H));
+      };
+  return (
+    <form
+        style={{
+          margin: "10px",
+          display: "flex",
+          gap: "20px",
+          flexDirection: "column",
+        }}
+        onSubmit={submit}
+      >
+        <h3>Inventario cero</h3>
+        <TextField
+          autoFocus={true}
+          value={ZI.PPT}
+          onChangeInputValue={onChange}
+          id={"D"}
+          title={"Demanda"}
+          type={"number"}
+          isRequired={true}
+          readOnly={false}
+        />
+
+       
+        <TextField
+          autoFocus={false}
+          onChangeInputValue={onChange}
+          type={"number"}
+          id={"S"}
+          isRequired={true}
+          title={"Coste de establecimiento"}
+          value={ZI.MESES}
+          readOnly={false}
+        />
+
+       
+        <Button title={"Calcular"} onSubmit={submit} />
+      </form>
+
   )
 }
 
