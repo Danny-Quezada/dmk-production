2c9cacc09e505af321ddebb85ec04635b8b12e09
Danny-Quezada <danny.quezada32u@std.uni.edu.ni>
2024-06-11 13:49:49 -0600

    Ending ZeroInventory's logic

--- src/lib/infrastructure/CALCULATEZI.ts
+++ src/lib/infrastructure/CALCULATEZI.ts
@@ -1,23 +1,39 @@
-export default class CALCULATEZI{
+import ZEROINVENTORY from "../domain/Models/Equations/ZEROINVENTORY";
 
-    CalculateZI( PPT: number, // Producción promedio por trabajador
-    MESES: number, // Meses que se hará el inventario
-    OAI: number, // Operarios actuales iniciales
-    CDT: number, // Costo de despedir un trabajador
-    CCT: number, // Costo de contratar un trabajador
-    DL: [],
-    D:[]
-     ){
-        for(let i=0; i<DL.length; i++){
-            let UO=DL[i]*D[i]; //Unidades por operario
-            let OR //Operarios requeridos;
+export default class CALCULATEZI {
+
+    CalculateZI(PPT: number, // Producción promedio por trabajador
+    CDTH:number,
+        OAI: number, // Operarios actuales iniciales
+        CDT: number, // Costo de despedir un trabajador
+        CCT: number, // Costo de contratar un trabajador
+        DL: number[],
+        D: number[]
+    ) : ZEROINVENTORY[]{
+        let OU:number = 0;
+        let zeros: ZEROINVENTORY[]=[];
+        for (let i = 0; i < DL.length; i++) {
+         
+            let UO:number = DL[i] * PPT; //Unidades por operario
+            let OR:number = Math.ceil(D[i] / UO); //Operarios requeridos
+
+            console.log(D[i],UO,OR);
+            let OA:number = i == 0 ? Number(OAI) : OU //Operarios actuales
+            let OC:number = OR > OA ? OR - OA : 0;  //Operarios contratados
+            let OD:number = OA > OR ? OA - OR : 0; // Operarios despedidos
+            OU =  OA + OC - OD;  // Operarios utilizados
+
+            zeros.push(new ZEROINVENTORY(DL[i],D[i],UO,OR,OA,OC,OD,OU,D[i],OC*CCT,CDT*OD,DL[i]*OU*CDTH))
+
+            
         }
 
+        return zeros;
 
 
     }
 
-    
+
 
 
 
--- src/pages/User/Calculator/ZeroInventory.tsx
+++ src/pages/User/Calculator/ZeroInventory.tsx
@@ -3,9 +3,18 @@ import React, { useState } from "react";
 import TextField from "../../../components/Textfield/TextField";
 import { GrLinkNext } from "react-icons/gr";
 import LUCTLCSTYLE from "./LUCLTC/LUCLTC.module.css";
+import ZEROINVENTORY from "../../../lib/domain/Models/Equations/ZEROINVENTORY";
+import CALCULATEZI from "../../../lib/infrastructure/CALCULATEZI";
 const ZeroInventory = () => {
+
+
+
+
+  const [Zeros, setZeros] = useState<ZEROINVENTORY[] | null>(null);
   const [nextForm, setForm] = useState(false);
+  const CalculateZero: CALCULATEZI=new CALCULATEZI();
   const [ZI, setZI] = useState({
+    CDTH: 0,
     PPT: 0, // Producción promedio por trabajador
     MESES: 0, // Meses que se hará el inventario
     OAI: 0, // Operarios actuales iniciales
@@ -16,6 +25,9 @@ const ZeroInventory = () => {
   const onChange = (e) => {
     setZI({ ...ZI, [e.target.name]: e.target.value });
     setForm(false);
+    if(Zeros!=null){
+      setZeros(null)
+    }
   };
 
   const submit = (event) => {
@@ -57,7 +69,7 @@ const ZeroInventory = () => {
         >
           <TextField
             autoFocus={true}
-            value={ ZI.PPT===0 ? "": ZI.PPT.toString()}
+            value={ZI.PPT === 0 ? "" : ZI.PPT.toString()}
             onChangeInputValue={onChange}
             id={"PPT"}
             title={"Producción por trabajador"}
@@ -66,14 +78,14 @@ const ZeroInventory = () => {
             readOnly={false}
           />
           <TextField
-          
+
             autoFocus={false}
             onChangeInputValue={onChange}
             type={"number"}
             id={"MESES"}
             isRequired={true}
             title={"Meses"}
-            value={ZI.MESES===0 ? "" : ZI.MESES.toString()}
+            value={ZI.MESES === 0 ? "" : ZI.MESES.toString()}
             readOnly={false}
           />
           <TextField
@@ -83,7 +95,7 @@ const ZeroInventory = () => {
             id={"OAI"}
             isRequired={true}
             title={"Operarios actuales"}
-            value={ZI.OAI===0 ? "" : ZI.OAI.toString()}
+            value={ZI.OAI === 0 ? "" : ZI.OAI.toString()}
             readOnly={false}
           />
         </div>
@@ -97,7 +109,7 @@ const ZeroInventory = () => {
         >
           <TextField
             autoFocus={true}
-            value={ZI.CDT===0 ? "" :ZI.CDT.toString()}
+            value={ZI.CDT === 0 ? "" : ZI.CDT.toString()}
             onChangeInputValue={onChange}
             id={"CDT"}
             title={"Costo de despedir un trabajador"}
@@ -112,9 +124,20 @@ const ZeroInventory = () => {
             id={"CCT"}
             isRequired={true}
             title={"Costo de contratar un trabajador"}
-            value={ZI.CCT ===0 ? "": ZI.CCT.toString()}
+            value={ZI.CCT === 0 ? "" : ZI.CCT.toString()}
             readOnly={false}
           />
+          <TextField
+            autoFocus={false}
+            onChangeInputValue={onChange}
+            type={"number"}
+            id={"CDTH"}
+            isRequired={true}
+            title={"Costo de trabajador por hornal"}
+            value={ZI.CDTH === 0 ? "" : ZI.CDTH.toString()}
+            readOnly={false}
+          />
+
         </div>
 
         <button className={LUCTLCSTYLE.info} type="submit">
@@ -122,10 +145,36 @@ const ZeroInventory = () => {
         </button>
       </form>
       {nextForm && (
-        <form style={{ display: "flex", flexDirection: "column", gap: "25px" }} 
-        onSubmit={(e)=>{
-          e.preventDefault();
-        }}>
+        <form style={{ display: "flex", flexDirection: "column", gap: "25px" }}
+          onSubmit={(e) => {
+            e.preventDefault();
+           
+            const formElements = e.currentTarget
+              .elements as typeof e.currentTarget.elements & {
+              [key: string]: HTMLInputElement;
+            };
+
+            const Ds= Array.from(
+              { length: ZI.MESES },
+              (_, i) => {
+               
+                const demanda = Number(formElements[`Demanda ${i}`].value);
+            
+                return demanda;
+              }
+            );
+            const DLs= Array.from(
+              { length: ZI.MESES },
+              (_, i) => {
+               
+                const DL = Number(formElements[`Labores ${i}`].value);
+            
+                return DL;
+              }
+            );
+           setZeros(CalculateZero.CalculateZI(ZI.PPT,ZI.CDTH,ZI.OAI, ZI.CDT,ZI.CCT,DLs,Ds))
+           console.log(Zeros);
+          }}>
           <div style={{ overflowX: "auto" }}>
             <table className={LUCTLCSTYLE.table}>
               <thead>
@@ -141,14 +190,14 @@ const ZeroInventory = () => {
                   {Array.from({ length: ZI.MESES }, (_, i) => (
                     <td key={`input: ${i}`}>
                       <input
-                      min={1}
-                      
-                      max={503}
-                       
+                        min={1}
+
+                        max={503}
+
                         onInvalid={(e) => {
                           // e.preventDefault();
                           const event = e.target as HTMLInputElement;
-                       
+
                           if (event.value === "") {
                             event.setCustomValidity("Campo requerido");
                           }
@@ -160,7 +209,7 @@ const ZeroInventory = () => {
                           if (!Number.isInteger(Number(event.value))) {
                             event.setCustomValidity("Solamente números enteros");
                           }
-                          if (Number(event.value)<=0) {
+                          if (Number(event.value) <= 0) {
                             event.setCustomValidity("Tiene que se mayor que 0");
                           }
                         }}
@@ -172,7 +221,7 @@ const ZeroInventory = () => {
                         required
                         onInput={(e) => {
                           const event = e.target as HTMLInputElement;
-                              event.setCustomValidity("")
+                          event.setCustomValidity("")
                           if (calculateBool) {
                             setCalculate(false);
                           }
@@ -189,14 +238,14 @@ const ZeroInventory = () => {
                   {Array.from({ length: ZI.MESES }, (_, i) => (
                     <td key={`input2: ${i}`}>
                       <input
-                      min={1}
-                      
-                      max={503}
-                       
+                        min={1}
+
+                        max={100000}
+
                         onInvalid={(e) => {
                           // e.preventDefault();
                           const event = e.target as HTMLInputElement;
-                       
+
                           if (event.value === "") {
                             event.setCustomValidity("Campo requerido");
                           }
@@ -208,7 +257,7 @@ const ZeroInventory = () => {
                           if (!Number.isInteger(Number(event.value))) {
                             event.setCustomValidity("Solamente números enteros");
                           }
-                          if (Number(event.value)<=0) {
+                          if (Number(event.value) <= 0) {
                             event.setCustomValidity("Tiene que se mayor que 0");
                           }
                         }}
@@ -220,7 +269,7 @@ const ZeroInventory = () => {
                         required
                         onInput={(e) => {
                           const event = e.target as HTMLInputElement;
-                              event.setCustomValidity("")
+                          event.setCustomValidity("")
                           if (calculateBool) {
                             setCalculate(false);
                           }
@@ -233,11 +282,183 @@ const ZeroInventory = () => {
               </tbody>
             </table>
           </div>
-          <button>
-            si
+          <button className={LUCTLCSTYLE.info} type="submit">
+            Siguiente
           </button>
         </form>
+
+
       )}
+       {Zeros != null && (
+        <div>
+          <div>
+            <table className={LUCTLCSTYLE.table}>
+             <thead>
+              <tr>
+              <th colSpan={ZI.MESES + 1}>Inventario cero</th>
+              </tr>
+             </thead>
+              <tbody>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Días laborales</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.DiasLaborados}
+                  </td>)
+                }
+                <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.DiasLaborados;
+                    }, 0)} 
+                  </td>
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Demanda</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.Demanda}
+                  </td>)
+                }
+                <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.Demanda;
+                    }, 0)}
+                  </td>
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Operarios requeridos</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.OperariosRequeridos}
+                  </td>)
+                }
+                  <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.OperariosRequeridos;
+                    }, 0)}
+                  </td>
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Operarios Actuales</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.OperariosActuales}
+                  </td>)
+                }
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Operarios contratados</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.OperariosContratados}
+                  </td>)
+                }
+                  <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.OperariosContratados;
+                    }, 0)}
+                  </td>
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Operarios despedidos</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.OperariosDespedidos}
+                  </td>)
+                }
+                  <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.OperariosDespedidos;
+                    }, 0)}
+                  </td>
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Operarios utilizados</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.OperariosUtilizados}
+                  </td>)
+                }
+                  <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.OperariosUtilizados;
+                    }, 0)}
+                  </td>
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Unidades Producidas</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.UnidadesProducidas}
+                  </td>)
+                }
+                  <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.UnidadesProducidas;
+                    }, 0)}
+                  </td>
+               </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+       {Zeros != null && (
+        <div>
+          <div>
+            <table className={LUCTLCSTYLE.table} style={{marginBottom: "10px"}}>
+             <thead>
+              <tr>
+              <th colSpan={ZI.MESES + 1}>Costo de plan agregado de producción</th>
+              </tr>
+             </thead>
+              <tbody>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Por contratar</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.CostoPorContratar}
+                  </td>)
+                }
+                <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.CostoPorContratar;
+                    }, 0)} 
+                  </td>
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Por despedir</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.CostoPorDespedir}
+                  </td>)
+                }
+                <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.CostoPorDespedir;
+                    }, 0)}
+                  </td>
+               </tr>
+               <tr>
+                <td style={{ fontSize: "13px", paddingRight: "10px" }}>Por mano de obra</td>
+                {
+                  Zeros.map((e)=> <td>
+                    {e.CostoManoObra}
+                  </td>)
+                }
+                  <td>
+                    {Zeros.reduce((total: number, objeto: ZEROINVENTORY) => {
+                      return total + objeto.CostoManoObra;
+                    }, 0)}
+                  </td>
+               </tr>
+               </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+
+
     </div>
   );
 };
